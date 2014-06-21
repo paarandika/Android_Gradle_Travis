58855222751c93f5a37fcee6518fee71abab34f8
paarandika <paarandika@gmail.com>
2014-06-21 22:46:50 +0530

    added tests again

---
+++ app/src/androidTest/java/fossasia/valentina/bodyapp/managers/test/UserManagerTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2014, Fashiontec (http://fashiontec.org)
+ * Licensed under LGPL, Version 3
+ */
+package fossasia.valentina.bodyapp.managers.test;
+
+import android.content.Context;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.AndroidTestCase;
+import android.test.RenamingDelegatingContext;
+
+import fossasia.valentina.bodyapp.db.DatabaseHandler;
+import fossasia.valentina.bodyapp.main.MainActivity;
+import fossasia.valentina.bodyapp.managers.UserManager;
+import fossasia.valentina.bodyapp.models.User;
+
+public class UserManagerTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    Context context;
+    UserManager um;
+    User user;
+
+    public UserManagerTest () {
+
+        super(MainActivity.class);
+    }
+
+    public void setUp(){
+        context = getActivity().getApplicationContext();
+        um=UserManager.getInstance(context);
+        assertNotNull("User Manger null",um);
+        user=new User("test_email","test_name","test_id");
+
+    }
+
+    public void testAddUser() throws Exception {
+        um.addUser(user);
+        assertNotNull(um.isUser(user));
+    }
+
+    public void testUnsetCurrent() throws Exception {
+        um.unsetCurrent();
+        assertNull(um.getCurrent());
+
+    }
+
+    public void testSetCurrent() throws Exception {
+        um.setCurrent(user);
+        assertNotNull(um.getCurrent());
+    }
+}
